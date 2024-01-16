4737829e8de6e3cff994751e024d31652301e377
Thana-ptz <154771726+Thana-ptz@users.noreply.github.com>
2024-01-16 20:39:53 +0700

    Update get-users.ts

--- server/api/get-users.ts
+++ server/api/get-users.ts
@@ -1,8 +1,5 @@
-// Import required modules
 import { createPool, sql } from '@vercel/postgres'
-import { defineNuxtPlugin } from 'nuxt'
 
-// Define the seed function
 async function seed() {
   const createTable = await sql`
     CREATE TABLE IF NOT EXISTS users (
@@ -12,26 +9,26 @@ async function seed() {
       image VARCHAR(255),
       "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
-  `
+    `
 
   console.log(`Created "users" table`)
 
   const users = await Promise.all([
     sql`
-      INSERT INTO users (name, email, image)
-      VALUES ('Guillermo Rauch', 'rauchg@vercel.com', 'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg')
-      ON CONFLICT (email) DO NOTHING;
-    `,
+          INSERT INTO users (name, email, image)
+          VALUES ('Guillermo Rauch', 'rauchg@vercel.com', 'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg')
+          ON CONFLICT (email) DO NOTHING;
+      `,
     sql`
-      INSERT INTO users (name, email, image)
-      VALUES ('Lee Robinson', 'lee@vercel.com', 'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg')
-      ON CONFLICT (email) DO NOTHING;
-    `,
+          INSERT INTO users (name, email, image)
+          VALUES ('Lee Robinson', 'lee@vercel.com', 'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg')
+          ON CONFLICT (email) DO NOTHING;
+      `,
     sql`
-      INSERT INTO users (name, email, image)
-      VALUES ('Steven Tey', 'stey@vercel.com', 'https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg')
-      ON CONFLICT (email) DO NOTHING;
-    `,
+          INSERT INTO users (name, email, image)
+          VALUES ('Steven Tey', 'stey@vercel.com', 'https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg')
+          ON CONFLICT (email) DO NOTHING;
+      `,
   ])
   console.log(`Seeded ${users.length} users`)
 
@@ -40,16 +37,16 @@ async function seed() {
     users,
   }
 }
-
-// Define the Nuxt plugin
-export default defineNuxtPlugin(async (nuxt) => {
+export default defineEventHandler(async () => {
   const startTime = Date.now()
   const db = createPool()
   try {
     const { rows: users } = await db.query('SELECT * FROM users')
     const duration = Date.now() - startTime
-    console.log('Retrieved users:', users)
-    console.log('Duration:', duration, 'ms')
+    return {
+      users: users,
+      duration: duration,
+    }
   } catch (error) {
     // @ts-ignore
     if (error?.message === `relation "users" does not exist`) {
@@ -60,8 +57,10 @@ export default defineNuxtPlugin(async (nuxt) => {
       await seed()
       const { rows: users } = await db.query('SELECT * FROM users')
       const duration = Date.now() - startTime
-      console.log('Retrieved users:', users)
-      console.log('Duration:', duration, 'ms')
+      return {
+        users: users,
+        duration: duration,
+      }
     } else {
       throw error
     }
